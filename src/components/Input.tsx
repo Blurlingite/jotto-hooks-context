bdf48bcc5172e43505f81085c38841cb10bc5900
Vishnu Dawah <vkdawah@gmail.com>
2020-06-21 18:52:38 -0400

    Code and Test input setting successcontext

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,7 +6,7 @@ import successContext from "../contexts/successContext";
 interface ComponentProps {
   secretWord: string;
 }
-function Input(props: ComponentProps) {
+function Input({ secretWord }: { secretWord: string }) {
   const language = React.useContext(languageContext);
   const [success, setSuccess]: any = successContext.useSuccess();
   const [currentGuess, setCurrentGuess] = React.useState("");
@@ -33,7 +33,11 @@ function Input(props: ComponentProps) {
           className="btn btn-primary mb-2"
           onClick={(evt) => {
             evt.preventDefault();
-            setCurrentGuess("");
+
+            if (currentGuess === secretWord) {
+              setSuccess(true);
+            }
+            setCurrentGuess(""); // clear input box
           }}
         >
           {stringsModule.getStringByLanguage(language, "submit")}
